672d4d57456b197c7a58b312fc00391e21d08da7
moritalous <moritalous@gmail.com>
2015-06-29 20:08:01 +0900

    SwipeRefreshの追加

--- app/src/main/java/forest/rice/field/k/preview/view/topChart/TopChartListFragment.java
+++ app/src/main/java/forest/rice/field/k/preview/view/topChart/TopChartListFragment.java
@@ -2,15 +2,17 @@
 package forest.rice.field.k.preview.view.topChart;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.Menu;
+import android.view.View;
 
 import forest.rice.field.k.R;
 import forest.rice.field.k.base.NavigationDrawerBaseInterface;
 import forest.rice.field.k.preview.entity.Tracks;
-import forest.rice.field.k.preview.view.base.BaseListFragment;
+import forest.rice.field.k.preview.view.base.SwipeRefreshListFragment;
 import forest.rice.field.k.preview.view.topChart.TopChartAsyncTask.TopChartAsyncTaskCallback;
 
-public class TopChartListFragment extends BaseListFragment implements TopChartAsyncTaskCallback {
+public class TopChartListFragment extends SwipeRefreshListFragment implements TopChartAsyncTaskCallback {
 
     TopChartAsyncTask task;
 
@@ -22,7 +24,34 @@ public class TopChartListFragment extends BaseListFragment implements TopChartAs
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initiateRefresh();
+
+        // BEGIN_INCLUDE (setup_refreshlistener)
+        /**
+         * Implement {@link SwipeRefreshLayout.OnRefreshListener}. When users do the "swipe to
+         * refresh" gesture, SwipeRefreshLayout invokes
+         * {@link SwipeRefreshLayout.OnRefreshListener#onRefresh onRefresh()}. In
+         * {@link SwipeRefreshLayout.OnRefreshListener#onRefresh onRefresh()}, call a method that
+         * refreshes the content. Call the same method in response to the Refresh action from the
+         * action bar.
+         */
+        setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initiateRefresh();
+            }
+        });
+        setColorSchemeResources(R.color.green, R.color.yellow, R.color.red, R.color.blue);
+        // END_INCLUDE (setup_refreshlistener)
+    }
+
+    private void initiateRefresh() {
         task = new TopChartAsyncTask();
         task.callback = this;
         task.execute();
@@ -57,5 +86,6 @@ public class TopChartListFragment extends BaseListFragment implements TopChartAs
         this.tracks = tracks;
         TopChartArrayAdapter adapter = new TopChartArrayAdapter(getActivity(), tracks);
         setListAdapter(adapter);
+        setRefreshing(false);
     }
 }
