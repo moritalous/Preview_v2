88b3e9460d4fa7272a1e0fe6509b6266c4023111
moritalous <moritalous@gmail.com>
2015-07-06 22:41:34 +0900

    Twitterコミット

--- app/src/main/java/forest/rice/field/k/preview/entity/TweetsWithTrack.java
+++ app/src/main/java/forest/rice/field/k/preview/entity/TweetsWithTrack.java
@@ -1,6 +1,37 @@
 package forest.rice.field.k.preview.entity;
 
+import android.os.AsyncTask;
+
 import java.util.ArrayList;
 
 public class TweetsWithTrack extends ArrayList<TweetWithTrack> {
+
+    private Tracks playingTracks = new Tracks();
+
+    @Override
+    public boolean add(final TweetWithTrack object) {
+
+        new AsyncTask<TweetWithTrack, Integer, TweetWithTrack>() {
+
+            @Override
+            protected TweetWithTrack doInBackground(TweetWithTrack... tweetWithTracks) {
+
+                Track track = tweetWithTracks[0].track();
+
+                if(track.get(Track.trackId).equals("DUMMY")) {
+                    // DUMMY
+                } else {
+                    playingTracks.add(track);
+                }
+
+                return null;
+            }
+        }.execute(object);
+
+        return super.add(object);
+    }
+
+    public Tracks getPlayableTracks(){
+        return playingTracks;
+    }
 }
