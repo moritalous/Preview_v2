320680df0b0a45b563afbfb1bfd9288ac31c0300
moritalous <moritalous@gmail.com>
2015-07-11 10:46:05 +0900

    URL変更

--- app/src/main/java/forest/rice/field/k/preview/request/ITunesAPILookupCollectionRequest.java
+++ app/src/main/java/forest/rice/field/k/preview/request/ITunesAPILookupCollectionRequest.java
@@ -4,6 +4,6 @@ public class ITunesAPILookupCollectionRequest extends  ITunesApiSearchRequest {
 
     public ITunesAPILookupCollectionRequest(String keyword) {
         super(keyword);
-        this.endpoint = "https://itunes.apple.com/lookup?country=jp&media=music&entity=album&lang=ja_jp&limit=200&id=%s";
+        this.endpoint = "https://itunes.apple.com/lookup?id=%s&country=JP&entity=album&sort=recent&limit=200";
     }
 }
